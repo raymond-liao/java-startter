a414e965e18982289598070c7959fa4d9771ea60
raymond <yaoyu.liao@highsoft.ltd>
2023-05-02 11:40:19 +0800

    Hello World!

---
+++ src/test/java/com/example/javastartter/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.javastartter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
